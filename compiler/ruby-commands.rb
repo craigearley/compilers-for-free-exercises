fe06455d978bceda849d88863837d6352f78653c
Craig Earley <io@craigearley.com>
2020-02-14 18:24:17 -0500

    adds compiler directory and initial definitions

---
+++ compiler/ruby-commands.rb
@@ -0,0 +1,95 @@
+Number	= Struct.new :value
+require('treetop')
+require('json')
+
+Treetop.load('simple.treetop')
+
+Boolean  = Struct.new :value
+Variable = Struct.new :name
+
+Add	  = Struct.new :left, :right
+Multiply  = Struct.new :left, :right
+LessThan  = Struct.new :left, :right
+
+Assign	= Struct.new :name, :expression
+If	= Struct.new :condition, :consequence, :alternative
+Sequence = Struct.new :first, :second
+While	 = Struct.new :condition, :body
+
+class Number
+	def evaluate(environment)
+		value
+	end
+	def to_javascript
+		"function (e) { return #{JSON.dump(value)}; }"
+	end
+end
+
+class Boolean
+	def evaluate(environment)
+		value
+	end
+	def to_javascript
+		"function (e) { return #{JSON.dump(value)}; }"
+	end
+end
+
+class Variable
+	def evaluate(environment)
+		environment[name]
+	end
+	def to_javascript
+		"function (e) { return #{JSON.dump(name)}; }"
+	end
+end
+
+class Add
+	def evaluate(environment)
+		left.evaluate(environment) + right.evaluate(environment)
+	end
+end
+
+class Multiply
+	def evaluate(environment)
+		left.evaluate(environment) * right.evaluate(environment)
+	end
+end
+
+class LessThan
+	def evaluate(environment)
+		left.evaluate(environment) < right.evaluate(environment)
+	end
+end
+
+class Assign
+	def evaluate(environment)
+		environment.merge({ name => expression.evaluate(environment) })
+	end
+end
+
+class Sequence
+	def evaluate(environment)
+		second.evaluate(first.evaluate(environment))
+	end
+end
+
+class If
+	def evaluate(environment)
+		if condition.evaluate(environment)
+			consequence.evaluate(environment)
+		else
+			alternative.evaluate(environment)
+		end
+	end
+end
+
+class While
+	def evaluate(environment)
+		if condition.evaluate(environment)
+			evaluate(body.evaluate(environment))
+		else
+			environment
+		end
+	end
+end
+
